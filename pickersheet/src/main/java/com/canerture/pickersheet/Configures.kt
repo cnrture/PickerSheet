fbb98434352b5364a3174cdb546a60ca9d224e6b
Caner Türe <cnrture@gmail.com>
2023-06-30 05:33:17 +0300

    add configures

---
+++ pickersheet/src/main/java/com/canerture/pickersheet/Configures.kt
@@ -0,0 +1,31 @@
+package com.canerture.pickersheet
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class ItemConfigure(
+    val color: Color = Color.Black,
+    val size: TextUnit = 14.sp,
+    val padding: Dp = 16.dp
+)
+
+data class TitleConfigure(
+    val color: Color = Color.Black,
+    val size: TextUnit = 16.sp,
+    val align: TextAlign = TextAlign.Start
+)
+
+data class PickerSheetColors(
+    val scrimColor: Color = Color.Gray.copy(alpha = 0.6f),
+    val backgroundColor: Color = Color.White
+)
+
+data class SelectedIconConfigure(
+    val icon: Int = R.drawable.ic_check,
+    val color: Color = Color.Black,
+    val size: Dp = 16.dp
+)
--- pickersheet/src/main/java/com/canerture/pickersheet/TextPickerSheet.kt
+++ pickersheet/src/main/java/com/canerture/pickersheet/TextPickerSheet.kt
@@ -1,6 +1,5 @@
 package com.canerture.pickersheet
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -22,15 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,15 +38,13 @@ fun TextPickerSheet(
     list: List<String>,
     title: String? = null,
     selectedItem: String? = null,
-    selectedIcon: Int = R.drawable.ic_check,
+    isDragIconEnabled: Boolean = true,
     dividerEnabled: Boolean = false,
     fontFamily: FontFamily = FontFamily.Default,
-    titleFontColor: Color = Color.Black,
-    titleFontSize: TextUnit = 16.sp,
-    itemFontColor: Color = Color.Black,
-    itemFontSize: TextUnit = 14.sp,
-    itemPadding: Dp = 16.dp,
-    iconSize: Dp = 16.dp,
+    titleConfigure: TitleConfigure = TitleConfigure(),
+    itemConfigure: ItemConfigure = ItemConfigure(),
+    sheetColors: PickerSheetColors = PickerSheetColors(),
+    selectedIconConfigure: SelectedIconConfigure = SelectedIconConfigure(),
     onItemClick: ((String) -> Unit)? = null,
     onDismiss: (() -> Unit)? = null
 ) {
@@ -59,6 +54,11 @@ fun TextPickerSheet(
     if (isOpen) {
         ModalBottomSheet(
             sheetState = sheetState,
+            scrimColor = sheetColors.scrimColor,
+            containerColor = sheetColors.backgroundColor,
+            dragHandle = {
+                if (isDragIconEnabled) BottomSheetDefaults.DragHandle()
+            },
             onDismissRequest = { onDismiss?.invoke() }
         ) {
             Column(
@@ -68,12 +68,13 @@ fun TextPickerSheet(
             ) {
                 title?.let {
                     Text(
-                        modifier = Modifier,
+                        modifier = Modifier.fillMaxWidth().padding(top = if (isDragIconEnabled) 0.dp else 12.dp),
                         text = title,
                         fontWeight = FontWeight.Bold,
-                        fontSize = titleFontSize,
+                        fontSize = titleConfigure.size,
                         fontFamily = fontFamily,
-                        color = titleFontColor
+                        color = titleConfigure.color,
+                        textAlign = titleConfigure.align
                     )
                 }
                 LazyColumn {
@@ -81,25 +82,26 @@ fun TextPickerSheet(
                         Row(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .clickable {
+                                .clickableWithoutRipple {
                                     selectedItem = item
                                     onItemClick?.invoke(item)
                                 }
-                                .padding(top = if (title != null && index == 0) itemPadding else 0.dp),
+                                .padding(top = if (title != null && index == 0) itemConfigure.padding else 0.dp),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Text(
                                 modifier = Modifier
-                                    .padding(vertical = itemPadding)
+                                    .padding(vertical = itemConfigure.padding)
                                     .weight(1f),
                                 text = item,
-                                fontSize = itemFontSize,
+                                fontSize = itemConfigure.size,
                                 fontFamily = fontFamily,
-                                color = itemFontColor
+                                color = itemConfigure.color
                             )
                             if (item == selectedItem) Icon(
-                                modifier = Modifier.size(iconSize),
-                                painter = painterResource(id = selectedIcon),
+                                modifier = Modifier.padding(end = 8.dp).size(selectedIconConfigure.size),
+                                painter = painterResource(id = selectedIconConfigure.icon),
+                                tint = selectedIconConfigure.color,
                                 contentDescription = null
                             )
                         }
